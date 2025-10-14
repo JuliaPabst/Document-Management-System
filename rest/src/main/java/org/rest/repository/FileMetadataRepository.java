66c642d7d7193f96262ba440ff57d9a8e59261bc
SofiaHKG <145010488+SofiaHKG@users.noreply.github.com>
2025-10-14 22:14:57 +0200

    Implement partial matching for author and fileType filters

--- rest/src/main/java/org/rest/repository/FileMetadataRepository.java
+++ rest/src/main/java/org/rest/repository/FileMetadataRepository.java
@@ -11,9 +11,11 @@ import java.util.List;
 @Repository
 public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
     
-    List<FileMetadata> findByAuthor(String author);
+    @Query("SELECT f FROM FileMetadata f WHERE f.author LIKE %:author%")
+    List<FileMetadata> findByAuthor(@Param("author") String author);
     
-    List<FileMetadata> findByFileType(String fileType);
+    @Query("SELECT f FROM FileMetadata f WHERE f.fileType LIKE %:fileType%")
+    List<FileMetadata> findByFileType(@Param("fileType") String fileType);
     
     @Query("SELECT f FROM FileMetadata f WHERE f.filename LIKE %:keyword% OR f.author LIKE %:keyword%")
     List<FileMetadata> searchByKeyword(@Param("keyword") String keyword);
