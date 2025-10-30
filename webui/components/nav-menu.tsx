d02e366eee011914b6cd57becfefa847b56a031b
Jules Pabst <julia.pabst@kununu.com>
2025-10-30 10:43:57 +0100

    Remove unused import

--- webui/components/nav-menu.tsx
+++ webui/components/nav-menu.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { FileText, Search, Upload, LayoutDashboard, MessageSquare, Menu, X } from 'lucide-react'
+import { Search, Upload, LayoutDashboard, MessageSquare, Menu, X } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
 const navItems = [
