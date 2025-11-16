6921e72869ec1810b6d058a964581c6e9f70cab3
Marvin <m.loehlein@gmx.at>
2025-11-17 00:23:37 +0100

    added filemessagedto in paperlessWorkers

---
+++ paperlessWorkers/src/main/java/org/workers/dto/FileMessageDto.java
@@ -0,0 +1,19 @@
+package org.workers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileMessageDto {
+    private Long id;
+    private String filename;
+    private String author;
+    private String fileType;
+    private Long size;
+    private Instant uploadTime;
+}
