23a131956c0defa73cb162c13aae803e16524fee
Marvin <m.loehlein@gmx.at>
2025-11-16 20:59:35 +0100

    added service to build messages for rabbitMQ

---
+++ rest/src/main/java/org/rest/service/MessageProducerService.java
@@ -0,0 +1,32 @@
+package org.rest.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.rest.dto.FileMessageDto;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MessageProducerService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.queue.ocr}")
+    private String ocrQueueName;
+
+    @Value("${rabbitmq.queue.genai}")
+    private String genaiQueueName;
+
+    public void sendToOcrQueue(FileMessageDto message) {
+        log.info("SENDING message to OCR queue: {}", message);
+        rabbitTemplate.convertAndSend(ocrQueueName, message);
+    }
+
+    public void sendToGenAiQueue(FileMessageDto message) {
+        log.info("SENDING message to GenAI queue: {}", message);
+        rabbitTemplate.convertAndSend(genaiQueueName, message);
+    }
+}
