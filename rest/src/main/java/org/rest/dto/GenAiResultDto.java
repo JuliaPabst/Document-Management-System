851b0220f23f7b79fa7023ad8ea4e5ebe70eeea7
SofiaHKG <145010488+SofiaHKG@users.noreply.github.com>
2025-11-25 16:07:04 +0100

    Add new Dtos for GenAI and OCR result processing

---
+++ rest/src/main/java/org/rest/dto/GenAiResultDto.java
@@ -0,0 +1,18 @@
+package org.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+// DTO for GenAI processing results received from GenAIWorker
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenAiResultDto {
+	private Long documentId;
+	private String objectKey;
+	private String summary;
+	private LocalDateTime processedAt;
+}
