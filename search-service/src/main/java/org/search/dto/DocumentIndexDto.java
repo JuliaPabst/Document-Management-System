975f23360c9d0c39e03c1269a1c6a06357bf945e
Marvin <m.loehlein@gmx.at>
2025-12-28 23:42:47 +0100

    added dtos for search-service

---
+++ search-service/src/main/java/org/search/dto/DocumentIndexDto.java
@@ -0,0 +1,31 @@
+package org.search.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentIndexDto {
+    private Long documentId;
+    private String filename;
+    private String author;
+    private String fileType;
+    private Long size;
+    private String objectKey;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime uploadTime;
+
+    private String extractedText;
+    private String summary;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime processedTime;
+}
---
+++ search-service/src/main/java/org/search/dto/SearchResponseDto.java
@@ -0,0 +1,21 @@
+package org.search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchResponseDto {
+    private List<SearchResultDto> results;
+    private Long totalHits;
+    private Integer page;
+    private Integer size;
+    private Integer totalPages;
+    private Long searchTimeMs;
+}
