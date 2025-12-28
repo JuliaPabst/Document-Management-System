ef7bab18e7dcff255331b29228f0750ffd84f4fe
Marvin <m.loehlein@gmx.at>
2025-12-28 23:42:20 +0100

    added rabbitmq config for search-service

---
+++ search-service/src/main/java/org/search/config/RabbitMqConfig.java
@@ -0,0 +1,34 @@
+package org.search.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.search-indexing}")
+    private String searchIndexingQueueName;
+
+    @Bean
+    public Queue searchIndexingQueue() {
+        return new Queue(searchIndexingQueueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
